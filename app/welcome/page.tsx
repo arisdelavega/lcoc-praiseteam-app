ae0fd6972c9e2d21c955c9c8da7a7a0847c48171
Aris Dela Vega <arisdelavega0601@gmail.com>
2026-01-22 16:49:07 +0800

    Update v25

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -28,7 +28,7 @@ export default function WelcomePage() {
         <img
           src="/logo.png"
           alt="Church Logo"
-          className="w-15 h-15 object-contain"
+          className="w-10 h-10 object-contain"
         />
       </div>
 
